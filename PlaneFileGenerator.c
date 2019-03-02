823627977dfb84838e922c776f91e539bfb3fd37
henriquejosefaria <henriquejosefaria@gmail.com>
2019-03-02 21:14:23 +0000

    update

--- PlaneFileGenerator.c
+++ PlaneFileGenerator.c
@@ -1,6 +1,11 @@
 design_plane(int f,const* char file){
     float c = side / 2.0;
-    char* str = malloc(sizeof(char)*1024*1024);
-    sprintf(str,"2\n1 2 3\n1 3 4\n4\n %.5f, 0.0, -%.5f\n-%.5f, 0.0, -%.5f\n-%.5f, 0.0,  %.5f\n %.5f, 0.0,  %.5f \n",c,c,c,c,c,c,c,c);
-    write_on_file(str,patch);
+    TAD_ARRAYLIST l = new ARRAYLIST(6);
+    addElem(l,POINT(c,0.0,-c));
+    addElem(l,POINT(-c,0.0,-c));
+    addElem(l,POINT(-c,0.0,c));
+    addElem(l,POINT(-c,0.0,c));
+    addElem(l,POINT(c,0.0,c));
+    addElem(l,POINT(c,0.0,-c));
+    filter(l);
 }
