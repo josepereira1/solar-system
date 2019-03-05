b9a915cc74f468d00f83b94efe06c09f7828650a
João Paulo Marques <36260306+marquinhos87@users.noreply.github.com>
2019-03-05 01:22:10 +0000

    Add files via upload

--- src/boxGenerator.cpp
+++ src/boxGenerator.cpp
@@ -10,81 +10,81 @@ TAD_ARRAY_LIST getPointsOfBox(float x, float y, float z, int divisions) {
 	float dx = x/divisions, dy = y/divisions, dz = z/divisions;//size of divisions
 	int i,j;//Variables to run faces
 	//frontal face
-	for(i = 0 ; i < divisions ; i++) {
-		for(j = 0 ; j < divisions ; j++) {
+	for (i = 0; i < divisions; i++) {
+		for (j = 0; j < divisions; j++) {
 			//1º triangle
-			addElem(points,POINT(xmin+(j+1)*dx,ymin+i*dy,zmax));
-			addElem(points,POINT(xmin+(j+1)*dx,ymin+(i+1)*dy,zmax));
-			addElem(points,POINT(xmin+j*dx,ymin+i*dy,zmax));
+			addElem(points,POINT(xmin + (j + 1)*dx, ymin + i * dy, zmax));
+			addElem(points,POINT(xmin + (j + 1)*dx, ymin + (i + 1)*dy, zmax));
+			addElem(points,POINT(xmin + j * dx, ymin + i * dy, zmax));
 			//2º triangle
-			addElem(points,POINT(xmin+j*dx,ymin+i*dy,zmax));
-			addElem(points,POINT(xmin+(j+1)*dx,ymin+(i+1)*dy,zmax));
-			addElem(points,POINT(xmin+j*dx,ymin+(i+1)*dy,zmax));
+			addElem(points,POINT(xmin + j * dx, ymin + i * dy, zmax));
+			addElem(points,POINT(xmin + (j + 1)*dx, ymin + (i + 1)*dy, zmax));
+			addElem(points,POINT(xmin + j * dx, ymin + (i + 1)*dy, zmax));
 		}
 	}
 	//rigth face
-	for(i = 0 ; i < divisions ; i++) {
-		for(j = 0 ; j < divisions ; j++) {
+	for (i = 0; i < divisions; i++) {
+		for (j = 0; j < divisions; j++) {
 			//1º triangle
-			addElem(points,POINT(xmax,ymin+i*dy,zmax-(j+1)*dz));
-			addElem(points,POINT(xmax,ymin+(i+1)*dy,zmax-(j+1)*dz));
-			addElem(points,POINT(xmax,ymin+i*dy,zmax-j*dz));
+			addElem(points,POINT(xmax, ymin + i * dy, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmax, ymin + i * dy, zmin + j * dz));
+			addElem(points,POINT(xmax, ymin + (i + 1)*dy, zmin + (j + 1)*dz));
 			//2º triangle
-			addElem(points,POINT(xmax,ymin+i*dy,zmax-j*dz));
-			addElem(points,POINT(xmax,ymin+(i+1)*dy,zmax-(j+1)*dz));
-			addElem(points,POINT(xmax,ymin+(i+1)*dy,zmax-j*dz));
+			addElem(points,POINT(xmax, ymin + (i + 1)*dy, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmax, ymin + i * dy, zmin + j * dz));
+			addElem(points,POINT(xmax, ymin + (i + 1)*dy, zmin + j * dz));
 		}
 	}
 	//up face
-	for(i = 0 ; i < divisions ; i++) {
-		for(j = 0 ; j < divisions ; j++) {
+	for (i = 0; i < divisions; i++) {
+		for (j = 0; j < divisions; j++) {
 			//1º triangle
-			addElem(points,POINT(xmax-j*dx,ymax,zmax-(j+1)*dz));
-			addElem(points,POINT(xmax-(j+1)*dx,ymax,zmax-(j+1)*dz));
-			addElem(points,POINT(xmax-j*dx,ymax,zmax-j*dz));
+			addElem(points,POINT(xmin + i * dx, ymax, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin + (i + 1)*dx, ymax, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin + i * dx, ymax, zmin + j * dz));
 			//2º triangle
-			addElem(points,POINT(xmax-j*dx,ymax,zmax-j*dz));
-			addElem(points,POINT(xmax-(j+1)*dx,ymax,zmax-(j+1)*dz));
-			addElem(points,POINT(xmax-(j+1)*dx,ymax,zmax-j*dz));
+			addElem(points,POINT(xmin + i * dx, ymax, zmin + j * dz));
+			addElem(points,POINT(xmin + (i + 1)*dx, ymax, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin + (i + 1)*dx, ymax, zmin + j * dz));
 		}
 	}
 	//back face
-	for(i = 0 ; i < divisions ; i++) {
-		for(j = 0 ; j < divisions ; j++) {
+	for (i = 0; i < divisions; i++) {
+		for (j = 0; j < divisions; j++) {
 			//1º triangle
-			addElem(points,POINT(xmax-(j+1)*dx,ymin+i*dy,zmin));
-			addElem(points,POINT(xmax-(j+1)*dx,ymin+(i+1)*dy,zmin));
-			addElem(points,POINT(xmax-j*dz,ymin+i*dy,zmin));
+			addElem(points,POINT(xmin + (j + 1)*dx, ymin + i * dy, zmin));
+			addElem(points,POINT(xmin + j * dz, ymin + i * dy, zmin));
+			addElem(points,POINT(xmin + (j + 1)*dx, ymin + (i + 1)*dy, zmin));
 			//2º triangle
-			addElem(points,POINT(xmax-j*dz,ymin+i*dy,zmin));
-			addElem(points,POINT(xmax-(j+1)*dx,ymin+(i+1)*dy,zmin));
-			addElem(points,POINT(xmax-j*dx,ymin+(i+1)*dy,zmin));
+			addElem(points,POINT(xmin + (j + 1)*dx, ymin + (i + 1)*dy, zmin));
+			addElem(points,POINT(xmin + j * dz, ymin + i * dy, zmin));
+			addElem(points,POINT(xmin + j * dx, ymin + (i + 1)*dy, zmin));
 		}
 	}
 	//left face
-	for(i = 0 ; i < divisions ; i++) {
-		for(j = 0 ; j < divisions ; j++) {
+	for (i = 0; i < divisions; i++) {
+		for (j = 0; j < divisions; j++) {
 			//1º triangle
-			addElem(points,POINT(xmin,ymin+i*dy,zmin+(j+1)*dz));
-			addElem(points,POINT(xmin,ymin+(i+1)*dy,zmin+(j+1)*dz));
-			addElem(points,POINT(xmin,ymin+i*dy,zmin+j*dz));
+			addElem(points,POINT(xmin, ymin + i * dy, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin, ymin + (i + 1)*dy, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin, ymin + i * dy, zmin + j * dz));
 			//2º triangle
-			addElem(points,POINT(xmin,ymin+i*dy,zmin+j*dz));
-			addElem(points,POINT(xmin,ymin+(i+1)*dy,zmin+(j+1)*dz));
-			addElem(points,POINT(xmin,ymin+(i+1)*dy,zmin+j*dz));
+			addElem(points,POINT(xmin, ymin + i * dy, zmin + j * dz));
+			addElem(points,POINT(xmin, ymin + (i + 1)*dy, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin, ymin + (i + 1)*dy, zmin + j * dz));
 		}
 	}
 	//down face
-	for(i = 0 ; i < divisions ; i++) {
-		for(j = 0 ; j < divisions ; j++) {
+	for (i = 0; i < divisions; i++) {
+		for (j = 0; j < divisions; j++) {
 			//1º triangle
-			addElem(points,POINT(xmin+j*dx,ymin,zmin+(j+1)*dz));
-			addElem(points,POINT(xmin+(j+1)*dx,ymin,zmin+(j+1)*dz));
-			addElem(points,POINT(xmin+j*dx,ymin,zmin+j*dz));
+			addElem(points,POINT(xmin + i * dx, ymin, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin + i * dx, ymin, zmin + j * dz));
+			addElem(points,POINT(xmin + (i + 1)*dx, ymin, zmin + (j + 1)*dz));
 			//2º triangle
-			addElem(points,POINT(xmin+j*dx,ymin,zmin+j*dz));
-			addElem(points,POINT(xmin+(j+1)*dx,ymin,zmin+(j+1)*dz));
-			addElem(points,POINT(xmin+(j+1)*dx,ymin,zmin+j*dz));
+			addElem(points,POINT(xmin + (i + 1)*dx, ymin, zmin + (j + 1)*dz));
+			addElem(points,POINT(xmin + i * dx, ymin, zmin + j * dz));
+			addElem(points,POINT(xmin + (i + 1)*dx, ymin, zmin + j * dz));
 		}
 	}
 	return points;
