035ef041b546c2f66c1ea3a40d86490b56fd5ef2
Marquinhos87 <joaomarquinhos8713@gmail.com>
2019-03-06 17:09:18 +0000

    modifiquei o list2file.cpp

--- src/list2file.cpp
+++ src/list2file.cpp
@@ -1,52 +1,35 @@
 #include "list2file.h"
 
-void list2file(TAD_ARRAY_LIST points, int positions[], int tam, const char* path) {
+int save_coordinates(TAD_POINT point, char* str, int deslocamento) {
+	char* tmp = (char*) malloc(256);
+	sprintf(tmp,"%.5f, %.5f, %.5f\n", getX(point),getY(point),getZ(point));
+	len = strlen(tmp);
+	sprintf(str+deslocamento,"%s",tmp);
+	deslocamento+=len;
+	free(tmp);
+	return deslocamento;
+}
+
+void list2file(TAD_ARRAY_LIST points, const char* path) {
 	FILE *file = fopen(path,"w");
 	if (file != NULL) {
 		char* str = (char*) malloc(1024*1024*10);//alloc memory to final string
 		int deslocamento = 0;//displacement
-		//put number of triangles
-		char* tmp = (char*) malloc(256);
-		sprintf(tmp,"%d\n",tam/3);
-		int len = strlen(tmp);
-		sprintf(str,"%s",tmp);
-		deslocamento += len;
-		free(tmp);
-		int i;
-		//put the positions
-		for(i=0 ; i<(tam-1) ; i+=3) {
-			char* tmp2 = (char*) malloc(256);
-			sprintf(tmp2,"%d, %d, %d\n",positions[i],positions[i+1],positions[i+2]);
-			len = strlen(tmp2);
-			sprintf(str+deslocamento,"%s",tmp2);
-			deslocamento+=len;
-			free(tmp2);
-		}
-		int size = getArraySize(points);
 		//put number of points
-		char* tmp3 = (char*) malloc(256);
-		sprintf(tmp3,"%d\n",size);
-		len = strlen(tmp3);
-		sprintf(str+deslocamento,"%s",tmp3);
+		int size = getArraySize(points);
+		char* tmp = (char*) malloc(256);
+		sprintf(tmp,"%d\n",size);
+		int len = strlen(tmp);
+		sprintf(str+deslocamento,"%s",tmp);
 		deslocamento+=len;
-		free(tmp3);
+		free(tmp);
 		//put the points (x,y,z)
-		for(i=0 ; i<(size-1) ; i++) {
+		for(int i=0 ; i<size ; i++) {
 			TAD_POINT point = (TAD_POINT) getElem(points,i);
-			char* tmp4 = (char*) malloc(256);
-			sprintf(tmp4,"%.5f, %.5f, %.5f\n", getX(point),getY(point),getZ(point));
-			len = strlen(tmp4);
-			sprintf(str+deslocamento,"%s",tmp4);
-			deslocamento+=len;
-			free(tmp4);
+			deslocamento = save_coordinates(point,str,deslocamento);
 		}
-		TAD_POINT point = (TAD_POINT) getElem(points,i);
-		char* tmp4 = (char*) malloc(256);
-		sprintf(tmp4,"%.5f, %.5f, %.5f",getX(point),getY(point),getZ(point));
-		len = strlen(tmp4);
-		sprintf(str+deslocamento,"%s",tmp4);
-		deslocamento+=len;
-		free(tmp4);
+		deslocamento--;
+		sprintf(str+deslocamento,"\0");
 		//save on file
         fputs(str,file);
         //close the file
