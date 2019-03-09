917df0699b3bdbd4626cdfb6676fb6ff470bd8ac
josepereira1 <joseandremp1@gmail.com>
2019-03-09 18:16:12 +0000

    bin para linux

---
+++ bin_linux/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jose/Desktop/projeto-cg/src/ArrayList.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/ArrayList.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/Point.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/Point.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/boxGenerator.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/boxGenerator.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/coneGenerator.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/coneGenerator.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/engine.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/engine.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/file2list.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/file2list.cpp.o"
+  "/home/jose/Desktop/projeto-cg/tinyxml/tinystr.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/home/jose/Desktop/projeto-cg/tinyxml/tinystr.cpp.o"
+  "/home/jose/Desktop/projeto-cg/tinyxml/tinyxml.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/home/jose/Desktop/projeto-cg/tinyxml/tinyxml.cpp.o"
+  "/home/jose/Desktop/projeto-cg/tinyxml/tinyxmlerror.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/home/jose/Desktop/projeto-cg/tinyxml/tinyxmlerror.cpp.o"
+  "/home/jose/Desktop/projeto-cg/tinyxml/tinyxmlparser.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/home/jose/Desktop/projeto-cg/tinyxml/tinyxmlparser.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/list2file.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/list2file.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/planeGenerator.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/planeGenerator.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/sphereGenerator.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/sphereGenerator.cpp.o"
+  "/home/jose/Desktop/projeto-cg/src/xmlParser.cpp" "/home/jose/Desktop/projeto-cg/bin_linux/CMakeFiles/engine.dir/xmlParser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jose/Desktop/projeto-cg/src/../include"
+  "/home/jose/Desktop/projeto-cg/src/../tinyxml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
