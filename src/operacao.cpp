d42f7c6ee7f86057a1b11ddd9c9fda2947852e0e
henriquejosefaria <henriquejosefaria@gmail.com>
2019-03-22 09:48:37 +0000

    update

---
+++ src/operacao.cpp
@@ -0,0 +1,23 @@
+
+using namespace std;
+
+class OPERATION
+ {
+ public:
+ 	char flag;
+    float x;
+    float y;
+    float z;
+    float ang;
+    OPERATION(char flag, float x, float y, float z, float angulo){
+    	this->flag = flag;
+     	this->x = x;
+     	this->y = y;
+     	this->z = z;
+     	this->ang = angulo;
+    }
+ };
+
+
+
+
