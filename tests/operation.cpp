91560a65802d8b8cf5152f3e543a41fef4e8a0e9
Marquinhos87 <a81826@alunos.uminho.pt>
2019-04-15 23:32:56 +0100

    não consegui testar o novo xmlParser.cpp, ele encontra-se na pasta teste e tem lá um ficheiro .xml próprio para testar, o ficheiro operation.cpp e .h tamebm foram alterados juntamente com o testeXML.cpp

--- tests/operation.cpp
+++
@@ -1,8 +0,0 @@
-#include <operation.h>
-
- Operation::Operation(char flag, vector<TAD_POINT> point, float angulo, int time){
-        this->flag = flag;
-        this->point = point;
-        this->ang = angulo;
-        this->time = time;
-    }
