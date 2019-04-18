79d3de5a70fbc01485461d5218f573a1fc0fba20
henriquejosefaria <henriquejosefaria@gmail.com>
2019-04-18 10:40:33 +0100

    update

--- src/engine.cpp
+++ src/engine.cpp
@@ -29,11 +29,17 @@ float py = 0.0;
 float pz = 0.0;
 int mode = GL_FILL;
 int face = GL_FRONT;
+GLuint buffers[4];
+float* vertexB1 = (float*) malloc(59400*3*sizeof(float));
+float* vertexB2 = (float*) malloc(5220*3*sizeof(float));
+float* vertexB3 = (float*) malloc(540*3*sizeof(float));
 
 Group group;
 map<string,Figura> figuras;
 
 void design(Group g){
+    int sphere;
+    int vertices;
     glPushMatrix();
 
     for(unsigned i = 0; i<g.operacoes.size() ;i++) {
@@ -58,14 +64,15 @@ void design(Group g){
 
     for(unsigned i = 0; i<g.ficheiros.size() ;i++) {
         string nome_ficheiro = g.ficheiros[i];
-        std::vector<float> pontos;
-        
-        pontos = figuras[nome_ficheiro].pontos;
+        if(nome_ficheiro.compare("sphere1.3d")) {sphere = 0; vertices=59400;}
+        else if(nome_ficheiro.compare("sphere2.3d")) {sphere = 1; vertices=5220;}
+        else {sphere=2; vertices=540;}
 
-        glBegin(GL_TRIANGLES);
-        for(unsigned i = 0; i < pontos.size(); i+=3 )
-            glVertex3f(pontos[i], pontos[i+1], pontos[i+2]);
-        glEnd();
+        glBindBuffer(GL_ARRAY_BUFFER,buffers[sphere]); // paga no buffer sphere
+        // nº de pontos para formar 1 vertice/ tipo da coordenada/ distancia entre indices dos vertices consecutivos / onde começa o array
+        glVertexPointer(3,GL_FLOAT,0,0);
+         // ultima variavel -> número de vertices não de triangulos
+        glDrawArrays(GL_TRIANGLES,0,vertices);
         //glEnable(GL_CULL_FACE);
     }
 
@@ -213,6 +220,78 @@ static void printFiguras(map<string,Figura> figuras) {
     }
 }
 
+// preenche VBO
+void getPointsOfSphere(float radius, int slices, int stacks) {
+    float* thisVector;
+    if(radius == 50) thisVector = vertexB1;
+    else if(radius == 10) thisVector = vertexB2;
+    else  thisVector = vertexB3;
+    float r1 = radius;
+    float r2 = radius;
+    float alfa = (2*M_PI)/slices;
+    float betaYoX = (M_PI)/stacks;
+    float height = radius;
+    float y1,y2;
+    int i;
+    int max = slices*(stacks-1)+2;
+    int m1,m2;
+    // tenho stack + 1 camadas para ligar, a 1ª e a ultima têm 1 vertice
+    for(m1=0,i=0;m1<stacks;m1++){ // vertices nas pontas = nº de slices
+        y2= height;
+        height = cos(betaYoX*(m1+1))*radius;
+        y1 = height;
+        r2 = sqrt((radius*radius)-(y2*y2));
+        r1 = sqrt((radius*radius)-(y1*y1));
+        for(m2=0;m2<slices;m2++,i+=9){ //  vertices interiores = nº de slices * 2
+            if(m1==0){
+                thisVector[i] = 0.0;
+                thisVector[i+1] = y2;
+                thisVector[i+2] = 0.0;
+                thisVector[i+3] = r1*cos((m2+1)*alfa;
+                thisVector[i+4] = y1;
+                thisVector[i+5] = r1*sin((m2+1)*alfa);
+                thisVector[i+6] = r1*cos(m2*alfa);
+                thisVector[i+7] = y1;
+                thisVector[i+8] = r1*sin(m2*alfa));
+
+            } else if(m1 == (stacks-1)){
+                thisVector[i] = 0.0;
+                thisVector[i+1] = y1;
+                thisVector[i+2] = 0.0;
+                thisVector[i+3] = r2*cos(m2*alfa);
+                thisVector[i+4] = y2;
+                thisVector[i+5] = r2*sin(m2*alfa);
+                thisVector[i+6] = r2*cos((m2+1)*alfa);
+                thisVector[i+7] = y2;
+                thisVector[i+8] = r2*sin((m2+1)*alfa);
+
+            } else{
+                thisVector[i] = r2*cos(m2*alfa);
+                thisVector[i+1] = y2;
+                thisVector[i+2] = r2*sin(m2*alfa);
+                thisVector[i+3] = r1*cos((m2+1)*alfa);
+                thisVector[i+4] = y1;
+                thisVector[i+5] = r1*sin((m2+1)*alfa);
+                thisVector[i+6] = r1*cos(m2*alfa);
+                thisVector[i+7] = y1;
+                thisVector[i+8] = r1*sin(m2*alfa);
+                i+=9;
+
+                // triangulo inverso
+                thisVector[i] = r2*cos(m2*alfa);
+                thisVector[i+1] = y2;
+                thisVector[i+2] = r2*sin(m2*alfa);
+                thisVector[i+3] = r2*cos((m2+1)*alfa);
+                thisVector[i+4] = y2;
+                thisVector[i+5] = r2*sin((m2+1)*alfa);
+                thisVector[i+6] = r1*cos((m2+1)*alfa);
+                thisVector[i+7] = y1;
+                thisVector[i+8] = r1*sin((m2+1)*alfa);
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     
     parse(group,figuras,"teapot.xml");
@@ -238,6 +317,18 @@ int main(int argc, char** argv) {
     //  OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    glGenBuffers(3, buffers);                 // gera-me 3 buffers
+    glBindBuffer(GL_ARRAY_BUFFER,buffers[0]); // pega no buffer 0
+    getPointsOfSphere(50,100,100);            // preeche array a copiar para buffer 0
+    glBufferData(GL_ARRAY_BUFFER,sizeof(float)*3*59400, vertexB1, GL_STATIC_DRAW); // preenche buffer 0
+    glBindBuffer(GL_ARRAY_BUFFER,buffers[1]); // pega no buffer 1
+    getPointsOfSphere(10,30,30);              // preeche array a copiar para buffer 1
+    glBufferData(GL_ARRAY_BUFFER,sizeof(float)*3*5220, vertexB2, GL_STATIC_DRAW); // preenche buffer 1
+    glBindBuffer(GL_ARRAY_BUFFER,buffers[2]); // pega no buffer 2
+    getPointsOfSphere(2,10,10);               // preeche array a copiar para buffer 2
+    glBufferData(GL_ARRAY_BUFFER,sizeof(float)*3*540, vertexB3, GL_STATIC_DRAW); // preenche buffer 2
 
     spherical2Cartesian();
 	
