6377b89baebb9dcee32ac7f54f75de1b38f01b0a
Marquinhos87 <joaomarques8713@gmail.com>
2019-04-19 12:07:15 +0100

    variaveis não utilizadas eliminadas do ficheiro filterPoints.cpp

--- src/filterPoints.cpp
+++ src/filterPoints.cpp
@@ -25,42 +25,33 @@ void filter(TAD_ARRAY_LIST points, TAD_ARRAY_LIST* withoutRepeated, int** positi
 }
 
 TAD_ARRAY_LIST removeRepeated(TAD_ARRAY_LIST points){
-	int i , j, size;
-	size = getArraySize(points);
+	int size = getArraySize(points);
 	TAD_ARRAY_LIST res = ARRAY_LIST(size);
-	for(j = 0, i = 0; i < size; i++){
+	for(int i = 0; i < size; i++){
 		TAD_POINT point = (TAD_POINT) getElem(points,i);
 		if(contains(res, point) == 0){
-			addElem(res, point);
-			j++;
+			addElem(res, point);	
 		}
 	}
 	return res;
 }
 int contains(TAD_ARRAY_LIST points, TAD_POINT point){
 	int size = getArraySize(points);
-	for(int i = 0; i < size; i++){
-		if(equalsPoint((TAD_POINT) getElem(points, i), point)){
-			return 1;
-		}
-	}
+	for(int i = 0; i < size; i++)
+		if(equalsPoint((TAD_POINT) getElem(points, i), point)) return 1;
 	return 0;	//	false
 }
 int* getPositions(TAD_ARRAY_LIST all_points, TAD_ARRAY_LIST filtered_points){
 	int size = getArraySize(all_points);
 	int* res = (int*)malloc(size*sizeof(int));
-	for(int i = 0; i < size; i++){
+	for(int i = 0; i < size; i++)
 		res[i] = getPosition(filtered_points, (TAD_POINT) getElem(all_points, i));
-	}
 	return res;
 }
 
 int getPosition(TAD_ARRAY_LIST points, TAD_POINT point){
 	int size = getArraySize(points);
-	TAD_POINT aux_point;
-	for(int i = 0; i < size; i++){
-		aux_point = (TAD_POINT) getElem(points, i);
-		if(equalsPoint(aux_point , point))return i;
-	}
+	for(int i = 0; i < size; i++)
+		if(equalsPoint((TAD_POINT) getElem(points, i), point)) return i;
 	return -1;
 }
