df008128f320d73bae7acb65fa5bf0dba5ffaac3
Marquinhos87 <joaomarques8713@gmail.com>
2019-05-10 16:16:25 +0100

    xmlParse modificado

--- include/xmlParser.h
+++ include/xmlParser.h
@@ -3,8 +3,10 @@
 
 #include <group.h>
 #include <map>
+#include <vector>
+#include <string>
 #include <Figura.h>
 
-void parse(Group &group, map<string,Figura> &figuras,int* nGrupos, const char* path);
+void parse(Group &group, map<string,Figura> &figuras, vector<string> textures, int* nGrupos, const char* path);
 
 #endif
